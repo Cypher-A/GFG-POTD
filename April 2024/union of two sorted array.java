af35123cb411f1013ce49c6782a4ef5958e82e92
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-04-20 23:46:03 +0530

    Update union of two sorted array.java

--- April 2024/union of two sorted array.java
+++ April 2024/union of two sorted array.java
@@ -1,1 +1,46 @@
+class Solution
+{
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        int i = 0, j = 0; 
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        
+        while (i < n && j < m) 
+        { 
+            while(  i+1<n  &&  arr1[i]==arr1[i+1]  )
+                i++;
+           while(  j+1<m  &&  arr2[j]==arr2[j+1]  )
+                j++;
+                
+            if (arr1[i] < arr2[j]) 
+                ans.add(arr1[i++]);
+          
+            else if (arr2[j] < arr1[i]) 
+                ans.add(arr2[j++]);
+                
+            else
+            {
+                ans.add(arr2[j++]);
+                i++;
+            }
+        } 
+        
+        while(i < n)
+        {
+            while(  i+1<n  &&  arr1[i]==arr1[i+1]  )
+                i++;
+            
+            ans.add(arr1[i++]);
+        }
+        while(j < m)
+        {
+            while(  j+1<m  &&  arr2[j]==arr2[j+1]  )
+                j++;
+            
+            ans.add(arr2[j++]);
+        }
+        
+        return ans;   
+        }
+}
 

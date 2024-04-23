dbd1598c08ab1e00ff405dd441f14ed0774eda77
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-04-23 17:24:53 +0530

    Update rohan's love for matrix.java

--- April 2024/rohan's love for matrix.java
+++ April 2024/rohan's love for matrix.java
@@ -1,1 +1,19 @@
+class Solution {
+    static int M = 1000000007; // modulo
 
+    // Function to calculate the first element of the Fibonacci sequence
+    static int firstElement(int n) {
+        int a = 0, b = 1, c, i;
+
+        if (n == 0) return a;
+
+        // Loop to calculate Fibonacci sequence
+        for (i = 2; i <= n; i++) {
+            c = (a + b) % M;
+            a = b % M;
+            b = c % M;
+        }
+
+        return b % M;
+    }
+}

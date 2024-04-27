53895060ba0d0f350637c651af3aa989062d1021
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-04-27 21:44:20 +0530

    Update merge sort on doubly linked list.java

--- April 2024/merge sort on doubly linked list.java
+++ April 2024/merge sort on doubly linked list.java
@@ -1,1 +1,51 @@
+class Solution {
+    public static Node sortDoubly(Node node) {
+        if (node == null || node.next == null) {
+            return node;
+        }
+
+        Node second = splitList(node);
+
+        node = sortDoubly(node);
+        second = sortDoubly(second);
+
+        return merge(node, second);
+    }
+
+    public static Node splitList(Node node) {
+      
+        Node fast = node, slow = node;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+      
+        Node temp = slow.next;
+        slow.next = null;
+        return temp;
+    }
+
+    public static Node merge(Node first, Node second) {
+        if (first == null) {
+            return second;
+        }
+        if (second == null) {
+            return first;
+        }
+
+        if (first.data < second.data) {
+            first.next = merge(first.next, second);
+            first.next.prev = first;
+            first.prev = null;
+            return first;
+          
+        } else {
+            second.next = merge(first, second.next);
+            second.next.prev = second;
+            second.prev = null;
+            return second;
+        }
+    }
+}
 

a5dec0ca311b62e5b90d6677d28030134ee565dc
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-03 21:32:57 +0530

    Update K distance from root.java

--- May 2024/K distance from root.java
+++ May 2024/K distance from root.java
@@ -1,1 +1,23 @@
-
+class Tree
+{
+    void KdistanceUtil(Node root,int k,ArrayList<Integer> nodes){
+        if(root == null){
+            return;
+        }
+        if(k == 0){
+            nodes.add(root.data);
+            return;
+        }
+        KdistanceUtil(root.left, k-1,nodes);
+        KdistanceUtil(root.right, k-1,nodes);
+    }
+    
+    ArrayList<Integer> Kdistance(Node root, int k)
+    {
+        ArrayList<Integer> nodes = new ArrayList<Integer>();
+        
+        KdistanceUtil(root,k,nodes);
+        
+        return nodes;
+    }
+}

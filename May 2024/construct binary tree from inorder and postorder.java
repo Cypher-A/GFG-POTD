510190363b81a27f3d1930d8a6516cf21256ee62
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-04 23:28:20 +0530

    Update construct binary tree from inorder and postorder.java

--- May 2024/construct binary tree from inorder and postorder.java
+++ May 2024/construct binary tree from inorder and postorder.java
@@ -1,1 +1,26 @@
-
+class GfG
+{
+    Node findtheNode(int[] in, int[] post, int start, int end, List<Integer> index) {
+        if (index.get(0) == -1 || start > end) return null;
+        int divfact = -1;
+        for (int i = start; i <= end; i++) {
+            if (post[index.get(0)] == in[i]) {
+                divfact = i;
+                break;
+            }
+        }
+        int t = index.get(0);
+        Node node = new Node(post[index.get(0)]);
+        index.remove(0);
+        index.add(t - 1);
+        node.right = findtheNode(in, post, divfact + 1, end, index);
+        node.left = findtheNode(in, post, start, divfact - 1, index);
+        return node;
+    }
+    
+    Node buildTree(int in[], int post[], int n) {
+        List<Integer> index = new ArrayList<>();
+        index.add(n - 1);
+        return findtheNode(in, post, 0, n - 1, index);
+    }
+}

5de1e7d521dac3e8202d63bb2bd536fe748678d0
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-08 01:19:13 +0530

    Update root to leaf paths.java

--- May 2024/root to leaf paths.java
+++ May 2024/root to leaf paths.java
@@ -1,1 +1,32 @@
-
+class Solution {
+    public ArrayList<ArrayList<Integer>> Paths(Node root){
+        int[] path=new int[1000]; 
+        ArrayList<ArrayList<Integer>> paths = new ArrayList<>();  
+        printPathsRecur(root, path, 0, paths);  
+        return paths; 
+    }
+    
+    public static void printPathsRecur(Node root, int[] path, int pathLen, ArrayList<ArrayList<Integer>> paths){
+        if(root==null)
+            return; 
+        
+        path[pathLen]=root.data; 
+        pathLen++;
+        
+        if(root.left==null&&root.right==null){
+            paths.add(printArray(path,pathLen));  
+            
+        }else{
+            printPathsRecur(root.left,path,pathLen,paths); 
+            printPathsRecur(root.right,path,pathLen,paths); 
+        }
+    }
+    
+    public static ArrayList<Integer> printArray(int[] path,int l){
+        ArrayList<Integer> p = new ArrayList<Integer>();
+        for(int i=0;i<l;i++)
+        p.add(path[i]);
+        return p;
+    }
+}
+        

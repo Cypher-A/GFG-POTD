d37d5c9eb1e29ded4b06e0c8e44c1ba1baef4d12
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-08 01:11:23 +0530

    Update reverse level order traversal.java

--- May 2024/reverse level order traversal.java
+++ May 2024/reverse level order traversal.java
@@ -1,1 +1,24 @@
-
+class Tree
+{
+    public ArrayList<Integer> reverseLevelOrder(Node node) 
+    {
+        Queue<Node> q = new LinkedList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        q.offer(node);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i =0; i<size; i++){
+                Node current = q.remove();
+                ans.add(current.data);
+                if(current.right != null){
+                    q.offer(current.right);
+                }
+                if(current.left != null){
+                    q.offer(current.left);
+                }
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}

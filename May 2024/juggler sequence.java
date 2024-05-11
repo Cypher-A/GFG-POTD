9b71b22b730b1960ab2abe1bb1cc9dd794d7e118
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-11 15:16:44 +0530

    Update juggler sequence.java

--- May 2024/juggler sequence.java
+++ May 2024/juggler sequence.java
@@ -1,1 +1,19 @@
+class Solution {
+    static List<Long> jugglerSequence(long n) {
+        List<Long> list = new ArrayList<>();
+        list.add(n);
+        
+        while(n != 1){
+            if((n & 1) == 0){
+                n = (long)Math.sqrt(n);
+                list.add(n);
+            }
+            else{
+                n = (long)Math.pow(n, 1.5);
+                list.add(n);
+            }
+        }
+        return list;
+    }
+}
 

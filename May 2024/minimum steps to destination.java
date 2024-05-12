eb6a5eec7ea6e313eca72078b37dcb83f56772f8
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-12 12:26:43 +0530

    Update minimum steps to destination.java

--- May 2024/minimum steps to destination.java
+++ May 2024/minimum steps to destination.java
@@ -1,1 +1,13 @@
+class Solution {
+    public int minSteps(int d) {
+        int steps = 0;
+        int position = 0;
 
+        while (position < d || (position - d) % 2 != 0) {
+            steps++;
+            position += steps;
+        }
+
+        return steps;
+    }
+}

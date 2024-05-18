2152ee92ed6b4a21aa2ae047ec388a5f463da854
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-18 15:20:14 +0530

    Update find the highest numbers.java

--- May 2024/find the highest numbers.java
+++ May 2024/find the highest numbers.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int findPeakElement(List<Integer> a) {
+        int left = 0;
+        int right = a.size() - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (a.get(mid) < a.get(mid + 1)) {
+                // Peak is in the right half
+                left = mid + 1;
+            } else {
+                // Peak is in the left half or at mid
+                right = mid;
+            }
+        }
+        
+        // When left == right, we have found the peak element
+        return a.get(left);
+    }
+}
 

a7b54691f46c9f9c6b4619fa736e1440d0ec734d
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-20 16:59:45 +0530

    Update modular exponentiation for large numbers.java

--- May 2024/modular exponentiation for large numbers.java
+++ May 2024/modular exponentiation for large numbers.java
@@ -1,1 +1,15 @@
-
+class Solution
+{
+    public long PowMod(long x, long n, long m)
+    {
+        long result=1;
+  		while(n>0)
+  		{
+  		    if(n % 2 == 1)
+  		        result=((result % m) * (x % m))%m;
+  		    x = (x*x) % m;
+  		    n = n/2;
+  		}
+  		return result;
+    }
+}

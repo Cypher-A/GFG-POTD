9977dd38c19d45d866052a1fbb2a069d98a5176d
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-23 16:35:21 +0530

    Update K-palindrome.java

--- May 2024/K-palindrome.java
+++ May 2024/K-palindrome.java
@@ -1,1 +1,22 @@
+class Solution {
 
+    public static int kPalindrome(String str, int n, int k) {
+        // Create a DP table
+        int[][] dp = new int[n][n];
+
+        // Fill the DP table
+        for (int len = 1; len < n; len++) {
+            for (int i = 0, j = len; j < n; i++, j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Check if the number of deletions required is within the limit k
+        return dp[0][n - 1] <= k ? 1 : 0;
+    }
+
+}

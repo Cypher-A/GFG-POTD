ad50dd96cfd3737e4baeec64b1dde6c4428dfab3
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-05-29 21:42:50 +0530

    Update geek and its game of coins.java

--- May 2024/geek and its game of coins.java
+++ May 2024/geek and its game of coins.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public static int findWinner(int n, int x, int y) {
+        boolean[] dp = new boolean[n + 1];
+        
+        dp[0] = false;
+        
+        for (int i = 1; i <= n; i++) {
+            if (i - 1 >= 0 && !dp[i - 1]) {
+                dp[i] = true;
+            } else if (i - x >= 0 && !dp[i - x]) {
+                dp[i] = true;
+            } else if (i - y >= 0 && !dp[i - y]) {
+                dp[i] = true;
+            } else {
+                dp[i] = false;
+            }
+        }
+        
+        return dp[n] ? 1 : 0;
+    }
+}

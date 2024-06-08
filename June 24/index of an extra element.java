7ab96465bff5d8896fdfc001ea776dcd0b296eee
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-06-08 16:28:21 +0530

    Update index of an extra element.java

--- June 24/index of an extra element.java
+++ June 24/index of an extra element.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public int findExtra(int n, int arr1[], int arr2[]) {
+        int left = 0;
+        int right = n - 2; // arr2 has n-1 elements
+        
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            
+            if (arr1[mid] == arr2[mid]) {
+                // Extra element is in the right half
+                left = mid + 1;
+            } else {
+                // Extra element is in the left half or at mid
+                right = mid - 1;
+            }
+        }
+        
+        // Left pointer should be at the extra element's index
+        return left;
+    }
+}

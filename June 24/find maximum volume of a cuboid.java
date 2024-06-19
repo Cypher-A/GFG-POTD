6da2d72ca1bb3310d551f2ef0a1319e8d606bab2
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-06-19 13:34:24 +0530

    Create find maximum volume of a cuboid.java

---
+++ June 24/find maximum volume of a cuboid.java
@@ -0,0 +1,13 @@
+class Solution {
+    double maxVolume(double perimeter, double area) {
+        double P = perimeter;
+        double A = area;
+        double sqrtTerm = Math.sqrt(P * P - 24 * A);
+        double dimension1 = (P - sqrtTerm) / 12.0;
+        double dimension2 = (P / 4.0) - 2 * dimension1;
+        double volume = Math.pow(dimension1, 2) * dimension2;
+        return Math.round(volume * 100.0) / 100.0;
+    }
+}
+
+ 

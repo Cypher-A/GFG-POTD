ddf8bb762f7cfde84f3c9d44c4cf4015c29729e5
Cypher-A <119620645+Cypher-A@users.noreply.github.com>
2024-07-18 23:52:25 +0530

    Create longest alternating subsequence.java

---
+++ July 2024/longest alternating subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int alternatingMaxLength(int[] arr) {
+        // code here
+        
+        int increasing = 1;
+        int decreasing = 1;
+        
+        for (int i = 1; i < arr.length; i++) {
+            
+            if (arr[i] > arr[i-1]) {
+                increasing = decreasing + 1;
+            }
+            else if (arr[i] < arr[i-1]) {
+                decreasing = increasing + 1;
+            }
+        }
+        return Math.max(increasing, decreasing);
+        
+    }
+}
